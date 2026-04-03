8d8c2e475dd69a13e5eab203d41e898d97d0ca65
mayurianu23 <tarlemayuri74@gmail.com>
2026-04-03 18:01:19 +0530

    my frist Basic program

---
+++ firstapp/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent'>
+      <h1 id='child1'>hiii</h1>
+      <h2 id='child2'>hiiii kaise ho sare</h2>
+    </div>
+  )
+}
+
+export default App
